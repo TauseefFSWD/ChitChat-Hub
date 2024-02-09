6303880d48d7547ad99c8b54e7944804c250c9dd
Tauseef Akbar Ali Shaikh <mohdtauseef.ma32@gmail.com>
2024-02-09 21:21:42 +0530

    updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("https://chat-app-yt.onrender.com", {
+      const socket = io("https://chitchat-hub.onrender.com", {
         query: {
           userId: authUser._id,
         },
